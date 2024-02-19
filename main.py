935a65edd1ea59d1c497f9cb517a3be0cb0b50de
mhassandata <mhassandata@gmail.com>
2024-02-19 15:49:51 +0500

    Updated the main.py file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from enum import Enum
 app = FastAPI()
 
 @app.get("/", description="This is get route", deprecated=True)
@@ -28,3 +28,21 @@ async def get_item(user_id: str):
 @app.get("/user/me")
 async def get_current_user():
     return {"Message" : "This is current user"}
+
+
+class FoodEnum(str, Enum):
+    fruits = "fruits"
+    vegetables = "vegetables"
+    dairy = "dairy"
+
+
+@app.get("/foods/{food_name}")
+async def get_food(food_name: FoodEnum):
+    if food_name == FoodEnum.vegetables:
+        return {"food_name" : food_name, "message": "You are healthy"}
+    if food_name.value == 'fruits':
+        return {
+            "food_name": food_name, 
+            "message": "You are still health but like sweet things"
+            }
+    return {"food_name": food_name, "message": "i like chocolate milk"}
